2e763cac5f623b6b377647a9c38021b64a1c0748
jonfry1175 <odiboen@gmail.com>
2024-03-20 00:43:19 +0700

    register without encrypt password

--- server/controllers/UserC.js
+++ server/controllers/UserC.js
@@ -1,14 +1,29 @@
 const { User } = require('../models')
 
+
+
 class UserController {
     static async getUser(req, res) {
         try {
             const result = await User.findAll()
             res.status(200).json(result)
-
+            
         } catch (error) {
             res.status(500).json(error.message)
             console.error(error.message)
+        }
+    }
+    
+    static async register(req, res) {
+        try {
+            const {username, password} = req.body
+            const result = await User.create({
+                username, password
+            })
+
+            res.status(201).json(result)
+        } catch (error) {
+            res.status(500).json(error.message)
         }
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const userRoute = require('express').Router()
 const {UserController} = require('../controllers')
 
 userRoute.get('/', UserController.getUser)
+userRoute.post('/register', UserController.register)
 
 
 module.exports = userRoute
